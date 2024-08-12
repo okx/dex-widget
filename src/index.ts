f81cb86bf6ad7bc6fce1bbbb51f3dc9546899880
wenxuan.feng <wenxuan.feng@okg.com>
2024-08-12 14:38:56 +0800

    chore: optimized code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export { createCowSwapWidget } from './cowSwapWidget';
 export type { CowSwapWidgetHandler } from './cowSwapWidget';
-export * from './types';
-export * from './messages';
-export * from './themeUtils';
 
 export * from './events';
 export * from './common';
+
+export * from './messages';
+export * from './themeUtils';
+export * from './types';
+export * from './urlUtils';
+export * from './verifyParamsUtils';
+export * from './widgetHelp';
--- src/widgetHelp.ts
+++ src/widgetHelp.ts
@@ -1,5 +1,4 @@
 import {
-    ChainName,
     TradeType,
     IWidgetParams,
     ITokenPair,
