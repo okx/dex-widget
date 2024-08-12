78ccbc9d668f273277eb7f68d2640643a766dcf4
wenxuan.feng <wenxuan.feng@okg.com>
2024-08-12 11:40:14 +0800

    chore: optimized code

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export type { CowSwapWidgetHandler } from './cowSwapWidget';
 export * from './types';
 export * from './messages';
 export * from './themeUtils';
+
+export * from './events';
+export * from './common';
