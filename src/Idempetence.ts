50d7b7d0e42014ec0a0ffab83698d6ddad21e06d
wenxuan.feng <wenxuan.feng@okg.com>
2024-08-16 18:34:18 +0800

    chore: optimized the error msg

--- src/Idempetence.ts
+++ src/Idempetence.ts
@@ -24,8 +24,24 @@ class Idempotence {
         console.log('handlePostMessage:', hasId, id, data);
 
         if (this.messageQueue.has(id)) {
-            console.log(`Message with id ${id} has already been sent.`);
-            return;
+            // check if the message has been sent twice
+            if (this.messageQueue.get(id)) {
+                console.log(`Message with id ${id} has already been sent.`);
+                return;
+            } else {
+                // Mark the message as sent twice
+                this.messageQueue.set(id, true);
+
+                // Send the message here
+                container.postMessage(data, "*");
+
+                // check size
+                if (this.messageQueue.size > 100) {
+                    this.messageQueue.clear();
+                }
+
+                return;
+            }
         }
 
         // Send the message here
@@ -37,7 +53,7 @@ class Idempotence {
         }
 
         // Mark the message as sent
-        this.messageQueue.set(id, true);
+        this.messageQueue.set(id, false);
     }
 
     // method to reset the processed ids and message queue
