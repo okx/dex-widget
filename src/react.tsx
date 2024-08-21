ead9d45143d249170da075e7145434c70b334eec
wenxuan.feng <wenxuan.feng@okg.com>
2024-08-22 03:34:03 +0800

    chore: change react hook

--- src/react.tsx
+++ src/react.tsx
@@ -1,10 +1,10 @@
-import React, { createContext, useContext, useRef, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useRef, useEffect, ReactNode, useState } from 'react';
 
-import { createOkSwapWidget, type OkSwapWidgetHandler, type IWidgetConfig } from './index';
+import { createOkxSwapWidget, type OkxSwapWidgetHandler, type IWidgetConfig } from './index';
 
 
 // 创建上下文
-const DexWidgetContext = createContext<OkSwapWidgetHandler | null>(null);
+const DexWidgetContext = createContext<OkxSwapWidgetHandler | null>(null);
 
 interface DexWidgetProviderProps {
     children: ReactNode;
@@ -13,20 +13,20 @@ interface DexWidgetProviderProps {
 
 export const DexWidgetProvider: React.FC<DexWidgetProviderProps> = ({ children, config }) => {
     const iframeContainerRef = useRef<HTMLDivElement>(null);
-    const widgetHandlerRef = useRef<OkSwapWidgetHandler | null>(null);
+    const [providerState, setProviderState] = useState<OkxSwapWidgetHandler | null>(null);
 
     useEffect(() => {
         if (iframeContainerRef.current) {
-            widgetHandlerRef.current = createOkSwapWidget(iframeContainerRef.current, config);
+            setProviderState(createOkxSwapWidget(iframeContainerRef.current, config));
         }
 
         return () => {
-            widgetHandlerRef.current?.destroy();
+            providerState?.destroy();
         };
     }, [config]);
 
     return (
-        <DexWidgetContext.Provider value={widgetHandlerRef.current}>
+        <DexWidgetContext.Provider value={providerState}>
             <div ref={iframeContainerRef} style={{ width: '100%', height: config.params.height }} />
             {children}
         </DexWidgetContext.Provider>
@@ -37,4 +37,4 @@ export const useDexWidget = () => {
     return useContext(DexWidgetContext);
 };
 
-export { createOkSwapWidget, OkSwapWidgetHandler };
+export { createOkxSwapWidget, OkxSwapWidgetHandler };
