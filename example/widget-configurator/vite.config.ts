e4842325672b9d69f407540fede9bbb41dff04a1
wenxuan.feng <wenxuan.feng@okg.com>
2024-08-30 18:00:32 +0800

    chore: update configuration file

--- example/widget-configurator/vite.config.ts
+++ example/widget-configurator/vite.config.ts
@@ -4,15 +4,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills({
-    include: ['stream'],
-    globals: {
-      Buffer: true,
-      global: true,
-      process: true,
-    },
-    protocolImports: true,
-  }), react()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodePolyfills({
+          include: ['stream'],
+          globals: {
+            Buffer: true,
+            global: true,
+            process: true,
+          },
+          protocolImports: true,
+        }),
+      ]
+    }
+  },
   server: {
     port: 4200,
     host: 'localhost',
