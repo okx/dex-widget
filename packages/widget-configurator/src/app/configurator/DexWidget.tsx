3059cc70ac5aba820cdda4adc1ab3132700e2b22
haoyang.feng <haoyang.feng@okg.com>
2024-09-25 17:39:01 +0800

    fix: add connect function deps

--- packages/widget-configurator/src/app/configurator/DexWidget.tsx
+++ packages/widget-configurator/src/app/configurator/DexWidget.tsx
@@ -81,7 +81,7 @@ export const DexWidget: FC<{params: OkxSwapWidgetProps['params']}> = forwardRef(
           });
         }
       };
-    });
+    }, [openConnectModal]);
 
     return (<div ref={widgetRef} />);
 })
